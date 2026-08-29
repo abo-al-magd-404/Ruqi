67fa570adece836b1ed24cde0bea428f97873014
ahmed mokhtar <mo879938@gmail.com>
2026-08-29 17:15:14 +0300

    Update main.js

--- back-end/main.js
+++ back-end/main.js
@@ -6,6 +6,15 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         instrument: ObserveInstrument,
     });
+    
+    // Configure Cross-Origin Resource Sharing (CORS)
+    // 'credentials: true' is required to allow cookies to be sent back and forth
+    app.enableCors({
+        origin: ['http://localhost:3000', 'domainfrontEndProduction'], 
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, 
+    });
+    
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,
         transform: true,
