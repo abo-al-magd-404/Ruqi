12bdeba0d586d5be9f2ae3c372e34f40bfb81880
Abo Al Magd <abo.al.magd.404@gmail.com>
2026-08-31 18:25:19 +0300

    Delete src directory

--- src/common/enums/user-status.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum UserStatus {
-  PENDING = 'PENDING',
-  ACTIVE = 'ACTIVE',
-  SUSPENDED = 'SUSPENDED',
-}
--- src/modules/auth/dto/resend-otp.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class ResendOtpDto {
-  @IsEmail({}, { message: 'البريد الإلكتروني غير صالح' })
-  @IsNotEmpty({ message: 'البريد الإلكتروني مطلوب' })
-  email!: string;
-}
