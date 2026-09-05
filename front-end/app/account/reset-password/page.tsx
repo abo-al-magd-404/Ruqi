d8d2441d70799af91e64e5a87e8d712f7803fbe7
Abo Al Magd <abo.al.magd.404@gmail.com>
2026-09-05 14:09:59 +0300

    Delete front-end directory

--- front-end/app/account/reset-password/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Suspense } from "react";
-import ResetPasswordContent from "@/components/account/auth-form/reset-password-content";
-
-export default function ResetPasswordForm() {
-  return <ResetPasswordContent />;
-}
--- front-end/app/support/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Support() {
-  return (
-    <div className="w-full h-screen flex flex-col flex-1 items-center justify-center">
-      <h1>صفحة الدعم</h1>
-    </div>
-  );
-}
