78ca6da2122290b47607b11f0d5990a72a2ccb0a
Abo Al Magd <abo.al.magd.404@gmail.com>
2026-09-20 19:25:38 +0300

    Update submit-answers.dto.ts

--- back-end/src/modules/progress/dto/submit-answers.dto.ts
+++ back-end/src/modules/progress/dto/submit-answers.dto.ts
@@ -3,7 +3,5 @@ import { IsArray, IsInt, Min } from "class-validator";
 export class SubmitAnswersDto {
   @IsArray()
   @IsArray({ each: true })
-  @IsInt({ each: true })
-  @Min(0, { each: true })
   answers: number[][];
 }
